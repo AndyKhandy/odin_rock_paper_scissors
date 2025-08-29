783ea6884632503e8201ac889ccc0608c9823435
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-08-28 22:00:36 -0500

    Create way to randomly get computer choice
    Used Math.random to get a number between 1-3.
    1 would be rock, 2 would be scissors, and 3
    would be paper.

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+let computer = getComputerChoice();
+console.log("Hello World! Computer chose" + computer);
+
+
+function getComputerChoice()
+{
+    let randomNumber = Math.floor(Math.random() * (3)) + 1;
+    let choice = "";
+
+    switch(randomNumber)
+    {
+        case 1:
+            choice = "rock";
+            break;
+        case 2:
+            choice = "paper";
+            break;
+        default:
+            choice = "scissors";
+    }
+
+    return choice
+}
