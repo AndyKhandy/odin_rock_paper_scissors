9c75bde81823cd5f3bf2d30f7912e36664f410f8
AndyKhandy <177374520+AndyKhandy@users.noreply.github.com>
2025-08-29 14:31:02 -0500

    Add alerts to improve user experience

--- main.js
+++ main.js
@@ -7,32 +7,36 @@ function playGame(){
     let humanScore = 0;
     let computerScore = 0;
 
-    for(let i = 0; i < 5; i++)
+    for(let i = 1; i <= 5; i++)
     {
         computer = getComputerChoice();
         player = getHumanChoice();
-        playRound(player, computer);
+        playRound(player, computer, i);
     }
 
-    function playRound(humanChoice, computerChoice)
+    function playRound(humanChoice, computerChoice, round)
 {
     humanChoice = humanChoice.toLowerCase();
     if(humanChoice === computerChoice)
     {
         console.log("It was a tie no one won!");
+        alert("It was a tie no one won! Round: " + round + "/5");
     }
     else if((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" &&  computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")){
         humanScore++;
         console.log("Player won! " + humanChoice + " beats " + computerChoice);
+        alert("Player won! " + humanChoice + " beats " + computerChoice + " Round: " + round + "/5");
     }
     else{
         computerScore++;
         console.log("Computer won! " + computerChoice + " beats " + humanChoice);
+        alert("Computer won! " + computerChoice + " beats " + humanChoice + " Round: " + round + "/5");
     }
 }
 
     let winner =  humanScore >= computerScore ? "Player" : "Computer";
     console.log("The winner is " + winner);
+    alert("The winner is " + winner + " Player got " + humanScore + " points || Computer got " + computerScore + " points");
 }
 
 
