5d5dccfd2760103ef27d54b3529434d93bdb4706
AndyKhandy <andytajuly30@gmail.com>
2025-09-13 19:01:31 -0500

    Added buttons

--- main.js
+++ main.js
@@ -1,43 +1,56 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorBtn = document.querySelector("#scissor");
 
 playGame();
+
 
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
 
-    let computer = "";
-    let player = "";
 
-    for(let i = 1; i <= 5; i++)
-    {
-        computer = getComputerChoice();
-        player = getHumanChoice();
-        playRound(player, computer, i);
-    }
+    rockBtn.addEventListener("click", () => 
+        playRound(getComputerChoice(), "rock")
+    );
 
-    function playRound(humanChoice, computerChoice, round)
+    paperBtn.addEventListener("click", () => 
+        playRound(getComputerChoice(), "paper")
+    );
+
+    scissorBtn.addEventListener("click", () => 
+        playRound(getComputerChoice(), "scissors")
+    );
+
+    const div = document.querySelector(".display");
+    const para = document.createElement("p");
+
+    function playRound(computerChoice, humanChoice)
 {
     humanChoice = humanChoice.toLowerCase();
     if(humanChoice === computerChoice)
     {
-        console.log("It was a tie no one won!");
-        alert("It was a tie no one won! Round: " + round + "/5");
+        para.textContent = "It was a tie no one won!";
     }
     else if((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" &&  computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "paper")){
         humanScore++;
-        console.log("Player won! " + humanChoice + " beats " + computerChoice);
-        alert("Player won! " + humanChoice + " beats " + computerChoice + " Round: " + round + "/5");
+        para.textContent = "Player won! " + humanChoice + " beats " + computerChoice;
     }
     else{
         computerScore++;
-        console.log("Computer won! " + computerChoice + " beats " + humanChoice);
-        alert("Computer won! " + computerChoice + " beats " + humanChoice + " Round: " + round + "/5");
+        para.textContent = "Computer won! " + computerChoice + " beats " + humanChoice;
     }
+
+    if(humanScore == 5 || computerScore == 5)
+    {
+        let winner =  humanScore >= computerScore ? "Player" : "Computer";
+        para.textContent += "The winner is " + winner + "|| Player got " + humanScore + " points || Computer got " + computerScore + " points";
+    }
+
+    div.appendChild(para);
 }
 
-    let winner =  humanScore >= computerScore ? "Player" : "Computer";
-    console.log("The winner is " + winner);
-    alert("The winner is " + winner + "|| Player got " + humanScore + " points || Computer got " + computerScore + " points");
+    
 }
 
 
